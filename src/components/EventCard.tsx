5bdcc81ca7613d4403aadb31795141fac26ac370
Egor Ushakov <witluv@icloud.com>
2025-02-12 19:46:12 +0300

    feat: add an event customisation component

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -2,15 +2,22 @@ import { Button, Card, Image, Typography } from 'antd';
 import styles from '../styles/EventCard.module.css';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
+import SettingsForm from './SettingsForm';
+import { useState } from 'react';
+import { EditOutlined } from '@ant-design/icons';
 
 function EventCard() {
+    const [isEditing, setIsEditing] = useState(false);
     const event = useSelector((state: RootState) => state.event);
 
     return (
         <div className={styles.container}>
             <Card
                 className={styles.card}
-                cover={<Image src={event.imageUrl} alt={Event} />}
+                cover={<Image src={event.imageUrl} alt="Event" />}
+                actions={[
+                    <EditOutlined key="edit" onClick={() => setIsEditing(true)} />,
+                ]}
             >
                 <Typography.Title level={4} className={styles.title}>
                     {event.title || 'No title'}
@@ -30,6 +37,8 @@ function EventCard() {
                     </Button>
                 )}
             </Card>
+
+            {isEditing && <SettingsForm onClose={() => setIsEditing(false)} />}
         </div>
     )
 }
